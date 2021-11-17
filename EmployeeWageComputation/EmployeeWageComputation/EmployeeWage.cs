4b05a6e82e89f8f4eef0e5ac80a7d20c3eca2e81
unknown <Saitharala@gmail.com>
2021-11-17 22:01:03 +0530

    [ Tharala SaiPrashanth] Added Refactor Method Feauture

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -6,31 +6,33 @@ namespace EmployeeWageComputation
 {
     class EmployeeWage
     {
-        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4, WORKING_DAYS = 20;
-        int totalEmpWage, empHrs;
-        public void MonthlyWage()
+        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2;
+        public static int MonthlyEmployeeWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            Random random = new Random();
-            for (int i = 0; i < WORKING_DAYS; i++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage;
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
+                totalWorkingDays++;
+                Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
-                    case IS_FULL_TIME:
-                        this.empHrs = FULL_WORKING_HRS;
-                        break;
                     case IS_PART_TIME:
-                        this.empHrs = PART_WORKING_HRS;
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
-                        this.empHrs = 0;
+                        empHrs = 0;
                         break;
                 }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Employee Hours : " + empHrs);
             }
-            Console.WriteLine(this.totalEmpWage = this.empHrs * WAGE_PER_HR);
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine(company + " Total Employee Wage : " + totalEmpWage);
+            return totalEmpWage;
         }
     }
 }
-
-
-
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,9 +6,9 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmployeeWage employee = new EmployeeWage();
-            employee . MonthlyWage();
+            Console.WriteLine("Welcome To Employee Wage Computation Program.");
+            EmployeeWage.MonthlyEmployeeWage("Brand Factory", 50, 20, 130);
+            EmployeeWage.MonthlyEmployeeWage("Dell Technologies", 90, 20, 150);
         }
     }
 }
